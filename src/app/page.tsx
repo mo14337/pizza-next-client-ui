7a25414181e62c181d32353a7f57f8a2036b1c25
mo14337 <mohitsingh14336@gmail.com>
2025-03-05 21:05:35 +0530

    change body background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    <>
+    <div className=" bg-white p-10">
       <p className="text-2xl">Heyy</p>
       <Button>Heyy</Button>
-    </>
+    </div>
   );
 }
