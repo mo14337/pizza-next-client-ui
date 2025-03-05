4c770a4130564716f6e790de02cd121af649ecd1
mo14337 <mohitsingh14336@gmail.com>
2025-03-05 20:49:06 +0530

    Add shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <>
-    <p className="text-2xl">Heyy</p>
+      <p className="text-2xl">Heyy</p>
+      <Button>Heyy</Button>
     </>
   );
 }
