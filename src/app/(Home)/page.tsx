7a1f3927985e3d74919322f3ba84dfb3fdfd2570
mo14337 <mohitsingh14336@gmail.com>
2025-03-06 20:59:05 +0530

    Added tabs

--- src/app/(Home)/page.tsx
+++ src/app/(Home)/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Image from "next/image";
 
 export default function Home() {
@@ -20,12 +21,28 @@ export default function Home() {
           </div>
           <div className=" ">
             <Image
-              src={"/pizz-main.jpg"}
+              src={"/pizza-main.png"}
               alt="main-image"
               width={400}
               height={400}
             />
           </div>
+        </div>
+      </section>
+      <section>
+        <div className=" container mx-auto py-12">
+          <Tabs defaultValue="pizza" className="w-[400px]">
+            <TabsList>
+              <TabsTrigger value="account" className=" text-md">
+                Pizza
+              </TabsTrigger>
+              <TabsTrigger value="password" className=" text-md">
+                Beverages
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="pizza">Pizza List</TabsContent>
+            <TabsContent value="beverages">BEvrages list</TabsContent>
+          </Tabs>
         </div>
       </section>
     </>
