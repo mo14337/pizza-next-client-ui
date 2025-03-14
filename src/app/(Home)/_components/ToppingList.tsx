7e6b71689a9c531e221a65f0b778a49c44c8ee93
mo14337 <mohitsingh14336@gmail.com>
2025-03-14 22:48:56 +0530

    fetch topping based on selected tenant id

--- src/app/(Home)/_components/ToppingList.tsx
+++ src/app/(Home)/_components/ToppingList.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import ToppingCard from "./ToppingCard";
+import { useSearchParams } from "next/navigation";
 
 export interface ITopping {
   _id: string;
@@ -19,12 +20,15 @@ const ToppingList = ({
   handleToppingClick: (arg: ITopping) => void;
 }) => {
   const [toppings, setToppings] = useState<ITopping[]>([]);
+  const searchParams = useSearchParams();
 
   useEffect(() => {
     async function fetchData() {
       try {
         const toppingResponse = await fetch(
-          `${process.env.NEXT_PUBLIC_BACKEND_URL}/catalog-service/topping`
+          `${
+            process.env.NEXT_PUBLIC_BACKEND_URL
+          }/catalog-service/topping?tenantId=${searchParams.get("tenant")}`
         );
         const toppings = await toppingResponse.json();
         setToppings(toppings.data);
@@ -33,24 +37,31 @@ const ToppingList = ({
       }
     }
     fetchData();
-  }, [setSelectedTopping]);
+  }, [setSelectedTopping, searchParams]);
   return (
     <section className=" mt-6">
       <h3>Extra Toppings</h3>
       <div className=" grid grid-cols-3 gap-4 mt-2">
-        {toppings?.map((topping) => {
-          if (!topping.isPublish) {
-            return;
-          }
-          return (
-            <ToppingCard
-              handleToppingClick={handleToppingClick}
-              selectedTopping={selectedTopping}
-              key={topping._id}
-              topping={topping}
-            />
-          );
-        })}
+        {toppings.length > 0 ? (
+          toppings?.map((topping) => {
+            if (!topping.isPublish) {
+              return;
+            }
+            return (
+              <ToppingCard
+                handleToppingClick={handleToppingClick}
+                selectedTopping={selectedTopping}
+                key={topping._id}
+                topping={topping}
+              />
+            );
+          })
+        ) : (
+          <span className=" whitespace-nowrap text-center block">
+            {" "}
+            No Toppings Available
+          </span>
+        )}
       </div>
     </section>
   );
